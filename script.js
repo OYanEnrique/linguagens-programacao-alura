8c4518e77ccec412adb552e4a1a7324ed4213792
OYanEnrique <enrique4work@gmail.com>
2025-11-21 15:21:36 -0300

    feat: implementa busca por filtro e carregamento automatico de dados

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function renderizarCards(dados){
         let article = document.createElement("article");
         article.innerHTML = `
         <h2>${dado.nome}</h2>
-        <p>${dado.ano}</p>
+        <p>${dado.data_criacao}</p>
         <p>${dado.descricao}</p>
         <a href="${dado.link}" target="_blank">Saiba mais</a>
         `
